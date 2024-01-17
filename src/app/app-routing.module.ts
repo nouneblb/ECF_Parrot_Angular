3fe708a205dc6e2e0c75530f7142b4ef7810746e
Nouné BILBOULIAN <nouneblb@gmail.com>
2024-01-17 18:36:36 +0100

    app routes for service page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ServiceComponent } from './service/service.component';
+
+const routes: Routes = [
+  { path: 'service', component: ServiceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
