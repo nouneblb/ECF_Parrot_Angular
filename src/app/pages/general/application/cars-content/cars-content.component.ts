677866f97834af2d12d71cae8e5a3fc2a60d99bd
Nouné BILBOULIAN <nouneblb@gmail.com>
2024-01-31 20:38:08 +0100

    form automatique par IDs

--- src/app/pages/general/application/cars-content/cars-content.component.ts
+++ src/app/pages/general/application/cars-content/cars-content.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CarsService } from '../cars.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,8 +12,15 @@ import { CarsService } from '../cars.service';
 
 export class CarsContentComponent implements OnInit {
   car: any; 
+  contactForm: FormGroup; 
 
-  constructor(private route: ActivatedRoute, private carsService: CarsService) {} 
+  constructor(private route: ActivatedRoute, private carsService: CarsService, private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  } 
   
   ngOnInit(): void {
     // observer les changements de paramètres de route
@@ -25,5 +33,12 @@ export class CarsContentComponent implements OnInit {
     // utiliser le service.ts pour récuperer les détails du véhicule selon l'id 
     this.car = this.carsService.getCarDetails(carId);
     }); 
+  }
+
+  submitForm() {
+    if (this.contactForm.valid) {
+      // soumission du form 
+      console.log('Formulaire soumis avec succès!', this.contactForm.value);
+    }
   }
 }
